7288f2719e7a19d6e1ee1abd01c3c8e2dd89a86f
jarvee <jarveezhang@gmail.com>
2018-11-30 00:08:52 -0500

    11.30 Updates

--- data_processing/script/index.js
+++ data_processing/script/index.js
@@ -1,48 +1,43 @@
 
 const fs = require('fs');
-/*
-station_location(data=>{
-    fs.writeFileSync(`${__dirname}/../data/station_location.json`, JSON.stringify(data, null, 2));
-});*/
-
-
 /**
  * [
- *  {
+ *   {
  *      name: xxxxxx,
  *      location: [],
- *      month_usage:[
- *          [,,,,,,,]
- *      ],
- *      week_usage:[
- *          [,,,,,,,]
- *      ],
- *      holiday:[
- * 
- *      ]
- *  }
+ *      data:{
+ *          sum:[],
+ *          diff:[],
+ *          return:[],
+ *          pick:[]
+ *      }
+ *   }
  * ]
  * 
  *  */
 function format_data(filename){
     const location = JSON.parse(fs.readFileSync(`${__dirname}/../data/station_location.json`));
-    const monthly_usage = JSON.parse(fs.readFileSync(`${__dirname}/../data/station_monthly_usage.json`));
-    const output = [];
+    const usage = JSON.parse(fs.readFileSync(`${__dirname}/../data/station_usage.json`));
+    const output = {};
     for (let name in location) {
         if (location.hasOwnProperty(name)) {
-            if(monthly_usage[name]){
-                let station_data = {
-                    name: name,
-                    location: location[name],
-                    month_usage: monthly_usage[name]
+            if(usage[name]){
+                let total = 0;
+                for(let i = 0; i < 24; i++){
+                    total += usage[name].sum[i];
                 }
-                output.push(station_data);
+                let station_data = {
+                    location: location[name],
+                    total: total,
+                    data: usage[name]
+                };
+                output[name] = station_data;
             }
         }
     }
     fs.writeFileSync(filename, JSON.stringify(output, null, 2));
 }
 
-format_data(`${__dirname}/../data/station.json`);
+format_data(`${__dirname}/../data/station_v4.json`);
 
 
